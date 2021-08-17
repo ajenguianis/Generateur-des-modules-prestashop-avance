27ae7f01de8007484d9a9291c2cb9502a8cb60d9
ajenguianis <aajengui@evolutive-business.com>
2021-08-17 16:02:49 +0200

    remove last same name

--- src/Controller/GeneratorController.php
+++ src/Controller/GeneratorController.php
@@ -10,6 +10,7 @@ use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use SplFileInfo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -76,6 +77,8 @@ class GeneratorController extends AbstractController
         $module_name = $request->request->get('module_name');
 
         $module_dir = $this->dir . '/' . $module_name;
+        $filesystem = new Filesystem();
+        $filesystem->remove($module_dir);
         if (!is_dir($module_dir) && !@mkdir($module_dir, 0777, true) && !is_dir($module_dir)) {
             throw new \RuntimeException(sprintf('Cannot create directory "%s"', $module_dir));
         }
