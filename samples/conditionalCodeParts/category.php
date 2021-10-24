ebe150bdf0cfb68318851fbad8472b85a2d0a4b4
ajenguianis <aajengui@evolutive-business.com>
2021-10-24 21:47:41 +0200

    update 241020212147

--- samples/conditionalCodeParts/category.php
+++ samples/conditionalCodeParts/category.php
@@ -8,7 +8,7 @@ public function getCategoryChoices($inputName)
     $tree->setUseCheckBox(false)
         ->setAttribute('is_category_filter', $root->id)
         ->setRootCategory($root->id)
-        ->setSelectedCategories(array((int)Configuration::get('CATEGORY_1'))) //if you wanted to be pre-carged
+        ->setSelectedCategories(array((int)Configuration::get('setting_name'))) //if you wanted to be pre-carged
         ->setInputName($inputName); //Set the name of input. The option "name" of $fields_form doesn't seem to work with "categories_select" type
     return $tree->render();
 }
