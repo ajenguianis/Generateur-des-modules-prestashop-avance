3664dc278920a82f3075e1fb39e3f1baa937dea2
ajenguianis <ajenguianis@users.noreply.github.com>
2023-04-06 00:06:21 +0000

    Apply php-cs-fixer changes

--- src/Controller/GeneratorController.php
+++ src/Controller/GeneratorController.php
@@ -67,7 +67,7 @@ class GeneratorController extends AbstractController
 
         $module_dir = $this->dir . '/' . $module_name;
         $filesystem = new Filesystem();
-        if(is_dir($module_dir)){
+        if(is_dir($module_dir)) {
             $filesystem->remove($module_dir);
         }
         if (file_exists($module_dir)) {
